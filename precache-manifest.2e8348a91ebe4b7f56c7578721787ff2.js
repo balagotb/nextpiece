722dd2ffde6c854504f78a3822c547827ccaf0e9
Bernard Balagot <balagotb@gmail.com>
2019-05-31 17:12:16 -0700

    Updates

---
+++ precache-manifest.2e8348a91ebe4b7f56c7578721787ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d54f351c459ce059bc6e3e3698e05bb",
+    "url": "/nextpiece/index.html"
+  },
+  {
+    "revision": "2af0a472c402623527cd",
+    "url": "/nextpiece/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "98e3737b5e9cae4036aa",
+    "url": "/nextpiece/static/css/main.94ac592e.chunk.css"
+  },
+  {
+    "revision": "2af0a472c402623527cd",
+    "url": "/nextpiece/static/js/2.45047ae2.chunk.js"
+  },
+  {
+    "revision": "98e3737b5e9cae4036aa",
+    "url": "/nextpiece/static/js/main.9dd9408b.chunk.js"
+  },
+  {
+    "revision": "4c52e52408ed116b4de6",
+    "url": "/nextpiece/static/js/runtime~main.412d44b1.js"
+  }
+]);
